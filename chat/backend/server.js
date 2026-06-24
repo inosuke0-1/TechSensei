d7ae283766625d9b35d359bed33b864d8e841cbc
Vitão_dev <javaliinosuke0@gmail.com>
2026-06-24 01:37:03 -0300

    Update CORS origin to allow production URL

--- chat/backend/server.js
+++ chat/backend/server.js
@@ -18,7 +18,7 @@ app.use("/auth", googleAuth);
 
 
 app.use(cors({
-  origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+  origin: ["https://techsensei-1.onrender.com"],
   methods: ["GET", "POST"],
   credentials: true
 }));
